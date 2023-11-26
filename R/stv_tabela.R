79fccf84eb4b09e09b9af1c1631d6b8e015fd150
CrisYiJung <136659335+CrisYiJung@users.noreply.github.com>
2023-11-26 15:15:45 -0300

    Add files via upload

---
+++ R/stv_tabela.R
@@ -0,0 +1,22 @@
+#' @title Tabela das Principais Estatísticas
+#'
+#' @param dados Vetor numérico d contendo os valores da série.
+#'
+#' @description
+#' Função que retorna uma tabela com as principais estatísticas da série temporal
+#'
+#' @return Uma tabela das principais estatísticas da série temporal.
+#'
+#' @export
+stv_tabela = function(dados){
+  if(class(dados) %in% c("numeric", "integer")){
+    max = max(dados)
+    min = min(dados)
+    med = mean(dados)
+    sd = sd(dados)
+    median = median(dados)
+    df = data.frame(Estatistica = c("Mínimo", "Máximo", "Desvio Padrão", "Mediana", "Média"),
+                    Valor = c(min, max, sd, median, med))
+  }else{stop("Erro: o argumento 'dados' precisa ser numérico.")}
+  return(df)
+}
